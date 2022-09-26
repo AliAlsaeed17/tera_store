8002e81f889751660dc33bc041c035d7301528e5
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-26 19:25:23 +0300

    Create styles manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,57 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:tera_store/presentation/fonts_manager.dart';
+
+TextStyle _getTextStyle(
+  double fontSize,
+  FontWeight fontWeight,
+  Color color,
+) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: FontConstants.fontFamily,
+    color: color,
+    fontWeight: fontWeight,
+  );
+}
+
+// regular style
+TextStyle getRegularStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.regular, color);
+}
+
+// medium style
+TextStyle getMediumStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+
+// light style
+TextStyle getLightStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.light, color);
+}
+
+// bold style
+TextStyle getBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.bold, color);
+}
+
+// semiBold style
+TextStyle getSemiBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.semiBold, color);
+}
