a020b148a253e3c6e6b734bcd3c565b4632b4037
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-27 22:07:56 +0300

    Add route generator class

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:tera_store/presentation/auth/forgot_password/forgot_password_view.dart';
+import 'package:tera_store/presentation/auth/login/login_view.dart';
+import 'package:tera_store/presentation/auth/register/register.dart';
+import 'package:tera_store/presentation/main/main_view.dart';
+import 'package:tera_store/presentation/store_details/store_details.dart';
+
+import '../splah/splash_view.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String loginRoute = "/login";
@@ -5,4 +14,38 @@ class Routes {
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: Text("No Route Found"),
+        ),
+        body: Center(
+          child: Text("No Route Found"),
+        ),
+      ),
+    );
+  }
 }
