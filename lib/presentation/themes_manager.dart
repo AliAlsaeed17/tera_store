fab33b5c991dc214b50dde44514396c1011cf20a
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-27 11:43:05 +0300

    Add text theme

--- lib/presentation/themes_manager.dart
+++ lib/presentation/themes_manager.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tera_store/presentation/colors_manager.dart';
+import 'package:tera_store/presentation/fonts_manager.dart';
+import 'package:tera_store/presentation/styles_manager.dart';
 import 'package:tera_store/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
@@ -9,7 +11,7 @@ ThemeData getApplicationTheme() {
     primaryColorLight: ColorManager.lightPrimary,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
-    splashColor: ColorManager.lightPrimary, // ripple effect
+    splashColor: ColorManager.lightPrimary, // ripple effect color
     // card view theme
     cardTheme: CardTheme(
       color: ColorManager.white,
@@ -17,11 +19,53 @@ ThemeData getApplicationTheme() {
       elevation: AppSize.s4,
     ),
     // app bar theme
-
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.lightPrimary,
+        titleTextStyle: getRegularStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s16,
+        )),
     // buttun theme
-
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(), // half circle in each side of the buttun
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.lightPrimary,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s17,
+        ),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
     // text theme
-
+    textTheme: TextTheme(
+        headline1: getSemiBoldStyle(
+          color: ColorManager.darkGrey,
+          fontSize: FontSize.s16,
+        ),
+        subtitle1: getMediumStyle(
+          color: ColorManager.lightGrey,
+          fontSize: FontSize.s14,
+        ),
+        caption: getRegularStyle(
+          color: ColorManager.grey1,
+          fontSize: FontSize.s12,
+        ),
+        bodyText1: getRegularStyle(color: ColorManager.grey),
+        displayLarge: getLightStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s22,
+        )),
     // input decoration theme (text form field)
   );
 }
