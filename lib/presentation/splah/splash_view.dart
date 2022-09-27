2e1eff20861e6d68780ef8e8107dc818a533950d
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-27 21:44:26 +0300

    Create splash view

---
+++ lib/presentation/splah/splash_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplahsView extends StatefulWidget {
+  const SplahsView({Key? key}) : super(key: key);
+
+  @override
+  State<SplahsView> createState() => _SplahsViewState();
+}
+
+class _SplahsViewState extends State<SplahsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
