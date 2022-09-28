b7155c34db47c0ded08aad976778eccd706b3cb8
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-28 11:52:51 +0300

    Add timer to splash screen

--- lib/presentation/splah/splash_view.dart
+++ lib/presentation/splah/splash_view.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tera_store/presentation/resources/assets_manager.dart';
 import 'package:tera_store/presentation/resources/colors_manager.dart';
+import 'package:tera_store/presentation/resources/constants_manager.dart';
+import 'package:tera_store/presentation/resources/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -10,6 +14,32 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(Duration(seconds: AppConstants.splashDelay), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(
+      context,
+      Routes.onBoardingRoute,
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
