3e25e1cbfe9802df84fcb970e4081b322a675b6c
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-28 12:10:50 +0300

    Add onBoarding view assets

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,5 +1,10 @@
-const IMAGE_FOLDER_PATH = 'assets/images';
+const imageFolderPath = 'assets/images';
 
 class ImageAssets {
-  static const String splashLogo = '$IMAGE_FOLDER_PATH/splash_logo.png';
+  static const String splashLogo = '$imageFolderPath/splash_logo.png';
+
+  static const String onBoardingLogo1 = '$imageFolderPath/onboarding_logo1.svg';
+  static const String onBoardingLogo2 = '$imageFolderPath/onboarding_logo2.svg';
+  static const String onBoardingLogo3 = '$imageFolderPath/onboarding_logo3.svg';
+  static const String onBoardingLogo4 = '$imageFolderPath/onboarding_logo4.svg';
 }
