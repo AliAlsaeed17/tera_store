b66bc0c4280449460b4cf807e1af07fd65ed9186
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-28 11:25:47 +0300

    Update splash view

--- lib/presentation/splah/splash_view.dart
+++ lib/presentation/splah/splash_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tera_store/presentation/resources/assets_manager.dart';
+import 'package:tera_store/presentation/resources/colors_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -10,6 +12,11 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: Center(
+        child: Image.asset(ImageAssets.splashLogo),
+      ),
+    );
   }
 }
