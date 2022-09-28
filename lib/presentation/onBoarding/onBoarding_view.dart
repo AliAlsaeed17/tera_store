0265e4310d5e6fd03c8c4846fb08f49296b9ee78
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-28 19:43:21 +0300

    Update onBoarding view

--- lib/presentation/onBoarding/onBoarding_view.dart
+++ lib/presentation/onBoarding/onBoarding_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tera_store/presentation/resources/assets_manager.dart';
+import 'package:tera_store/presentation/resources/colors_manager.dart';
+import 'package:tera_store/presentation/resources/strings_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -8,8 +12,85 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnboardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+
+  final PageController _pageController = PageController();
+
+  int _currentPageIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(
+          title: AppStrings.onBoardingTitle1,
+          subTitle: AppStrings.onBoardingSubTitle1,
+          image: ImageAssets.onBoardingLogo1,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle2,
+          subTitle: AppStrings.onBoardingSubTitle2,
+          image: ImageAssets.onBoardingLogo2,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle3,
+          subTitle: AppStrings.onBoardingSubTitle3,
+          image: ImageAssets.onBoardingLogo3,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle4,
+          subTitle: AppStrings.onBoardingSubTitle4,
+          image: ImageAssets.onBoardingLogo4,
+        ),
+      ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return Container();
+        },
+      ),
+    );
   }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  final SliderObject _sliderObject;
+  const OnBoardingPage(this._sliderObject, Key? key) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 3,
+        ),
+      ],
+    );
+  }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject({
+    required this.title,
+    required this.subTitle,
+    required this.image,
+  });
 }
