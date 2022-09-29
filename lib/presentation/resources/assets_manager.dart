11180b21a2131c9debc2660d690b04dddcf2baa8
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-09-29 18:14:56 +0300

    Add onBoarding assets

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -7,4 +7,9 @@ class ImageAssets {
   static const String onBoardingLogo2 = '$imageFolderPath/onboarding_logo2.svg';
   static const String onBoardingLogo3 = '$imageFolderPath/onboarding_logo3.svg';
   static const String onBoardingLogo4 = '$imageFolderPath/onboarding_logo4.svg';
+
+  static const String hollowCirlceIc = '$imageFolderPath/hollow_cirlce_ic.svg';
+  static const String solidCircleIc = '$imageFolderPath/solid_circle_ic.svg';
+  static const String leftArrowIc = '$imageFolderPath/left_arrow_ic.svg';
+  static const String rightArrowIc = '$imageFolderPath/right_arrow_ic.svg';
 }
