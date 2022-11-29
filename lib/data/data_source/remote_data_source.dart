d4267d5c0c53529b6c8c8a0aaa0ede077796c46a
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-11-29 14:57:21 +0300

    Add remote data source implementation

--- lib/data/data_source/remote_data_source.dart
+++ lib/data/data_source/remote_data_source.dart
@@ -1,7 +1,20 @@
+import 'package:tera_store/data/network/app_api.dart';
 import 'package:tera_store/data/network/requests.dart';
 import 'package:tera_store/data/reposponse/responses.dart';
 import 'package:tera_store/domain/models/model.dart';
 
 abstract class RemoteDataSource {
   Future<AuthenticationResponse> login(LoginRequest loginRequest);
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource {
+  final AppServiceClient _appServiceClient;
+
+  RemoteDataSourceImpl(this._appServiceClient);
+
+  @override
+  Future<AuthenticationResponse> login(LoginRequest loginRequest) async {
+    return await _appServiceClient.login(
+        loginRequest.email, loginRequest.password);
+  }
 }
