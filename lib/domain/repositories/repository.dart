2506c5e87f478e21fc7b05dfd04a060cfa368263
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-11-29 14:39:41 +0300

    Add login function with either functionality

--- lib/domain/repositories/repository.dart
+++ lib/domain/repositories/repository.dart
@@ -1,3 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:tera_store/data/network/fauilure.dart';
+import 'package:tera_store/data/network/requests.dart';
+import 'package:tera_store/domain/models/model.dart';
+
 abstract class Repository {
-  login();
+  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
 }
