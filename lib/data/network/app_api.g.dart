3dd6f7e509bf6966f23c5e67948cdb943c92e5b3
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-11-29 13:01:10 +0300

    Generate login functionality

--- lib/data/network/app_api.g.dart
+++ lib/data/network/app_api.g.dart
@@ -17,6 +17,22 @@ class _AppServiceClient implements AppServiceClient {
 
   String? baseUrl;
 
+  @override
+  Future<AuthenticaionResponse> login() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<AuthenticaionResponse>(
+            Options(method: 'POST', headers: _headers, extra: _extra)
+                .compose(_dio.options, '/customers/login',
+                    queryParameters: queryParameters, data: _data)
+                .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = AuthenticaionResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
