86db1cb30f4c554001212e0cd600d83c03e04748
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-11-29 12:56:53 +0300

    Add login api call

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:tera_store/app/constants.dart';
+import 'package:tera_store/data/reposponse/responses.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String? baseUrl}) = _AppServiceClient;
+
+  @POST('/customers/login')
+  Future<AuthenticaionResponse> login();
+}
