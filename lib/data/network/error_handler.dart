922258e6597dbfc0cd8dff3116ab43d39e8364cf
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-11-30 11:17:48 +0300

    Adding response messages

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -2,7 +2,7 @@ enum DataSource {
   SUCCESS,
   NO_CONTENT,
   BAD_REQUEST,
-  FORBIDEN,
+  FORBBIDEN,
   UNAUTHORISED,
   NOTFOUND,
   INTERNAL_SERVER_ERROR,
@@ -18,7 +18,7 @@ class ResponeCode {
   static const int SUCCESS = 200; // success with data
   static const int NO_CONTENT = 201; // success with no data
   static const int BAD_REQUEST = 400; // failure , API rejected request
-  static const int FORBIDEN = 403; // failure , API rejected request
+  static const int FORBBIDEN = 403; // failure , API rejected request
   static const int UNAUTHORISED = 401; // failure , API is not authorised
   //static const int NOTFOUND = 200;
   static const int INTERNAL_SERVER_ERROR =
@@ -32,4 +32,28 @@ class ResponeCode {
   static const int CACHE_ERROR = -5;
   static const int NO_INTERNET_CONNECTION = -6;
   static const int UNKNOWN = -7;
+}
+
+class ResponeMessage {
+  static const String SUCCESS = "success"; // success with data
+  static const String NO_CONTENT = "sucess"; // success with no data
+  static const String BAD_REQUEST =
+      "Bad rquest ,Try again later"; // failure , API rejected request
+  static const String FORBBIDEN =
+      "Forbbiden rquest ,Try again later"; // failure , API rejected request
+  static const String UNAUTHORISED =
+      "User unauthorised ,Try again later"; // failure , API is not authorised
+  //static const int NOTFOUND = 200;
+  static const String INTERNAL_SERVER_ERROR =
+      "Something went wrong ,Try again later"; // failure , crash in server side
+
+  // local states code
+  static const String CONNECT_TIMEOUT = "Timeout error ,Try again later";
+  static const String CANCEL = "Request was canceled ,Try again later";
+  static const String RECIEVE_TIMEOUT = "Timeout error ,Try again later";
+  static const String SEND_TIMEOUT = "Timeout error ,Try again later";
+  static const String CACHE_ERROR = "Cache error ,Try again later";
+  static const String NO_INTERNET_CONNECTION =
+      "Please check your internet connection";
+  static const String UNKNOWN = "Something went wrong ,Try again later";
 }
