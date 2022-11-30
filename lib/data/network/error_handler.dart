202b21a9f052ea7e67f667d5093d858ffecf14bf
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-11-30 11:38:31 +0300

    Adding data source extension to get failure

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -1,3 +1,5 @@
+import 'package:tera_store/data/network/fauilure.dart';
+
 enum DataSource {
   SUCCESS,
   NO_CONTENT,
@@ -20,7 +22,7 @@ class ResponeCode {
   static const int BAD_REQUEST = 400; // failure , API rejected request
   static const int FORBBIDEN = 403; // failure , API rejected request
   static const int UNAUTHORISED = 401; // failure , API is not authorised
-  //static const int NOTFOUND = 200;
+  static const int NOTFOUND = 404; // failure , not found
   static const int INTERNAL_SERVER_ERROR =
       500; // failure , crash in server side
 
@@ -43,7 +45,8 @@ class ResponeMessage {
       "Forbbiden rquest ,Try again later"; // failure , API rejected request
   static const String UNAUTHORISED =
       "User unauthorised ,Try again later"; // failure , API is not authorised
-  //static const int NOTFOUND = 200;
+  static const String NOTFOUND =
+      "Something went wrong ,Try again later"; // failure , not found
   static const String INTERNAL_SERVER_ERROR =
       "Something went wrong ,Try again later"; // failure , crash in server side
 
@@ -56,4 +59,69 @@ class ResponeMessage {
   static const String NO_INTERNET_CONNECTION =
       "Please check your internet connection";
   static const String UNKNOWN = "Something went wrong ,Try again later";
+}
+
+extension DataSourceExtension on DataSource {
+  Failure getFailure() {
+    switch (this) {
+      case DataSource.SUCCESS:
+        return Failure(
+            code: ResponeCode.SUCCESS, message: ResponeMessage.SUCCESS);
+        break;
+      case DataSource.NO_CONTENT:
+        return Failure(
+            code: ResponeCode.NO_CONTENT, message: ResponeMessage.NO_CONTENT);
+        break;
+      case DataSource.BAD_REQUEST:
+        return Failure(
+            code: ResponeCode.BAD_REQUEST, message: ResponeMessage.BAD_REQUEST);
+        break;
+      case DataSource.FORBBIDEN:
+        return Failure(
+            code: ResponeCode.FORBBIDEN, message: ResponeMessage.FORBBIDEN);
+        break;
+      case DataSource.UNAUTHORISED:
+        return Failure(
+            code: ResponeCode.UNAUTHORISED,
+            message: ResponeMessage.UNAUTHORISED);
+        break;
+      case DataSource.NOTFOUND:
+        return Failure(
+            code: ResponeCode.NOTFOUND, message: ResponeMessage.NOTFOUND);
+        break;
+      case DataSource.INTERNAL_SERVER_ERROR:
+        return Failure(
+            code: ResponeCode.INTERNAL_SERVER_ERROR,
+            message: ResponeMessage.INTERNAL_SERVER_ERROR);
+        break;
+      case DataSource.CONNECT_TIMEOUT:
+        return Failure(
+            code: ResponeCode.CONNECT_TIMEOUT,
+            message: ResponeMessage.CONNECT_TIMEOUT);
+        break;
+      case DataSource.CANCEL:
+        return Failure(
+            code: ResponeCode.CANCEL, message: ResponeMessage.CANCEL);
+        break;
+      case DataSource.RECIEVE_TIMEOUT:
+        return Failure(
+            code: ResponeCode.RECIEVE_TIMEOUT,
+            message: ResponeMessage.RECIEVE_TIMEOUT);
+        break;
+      case DataSource.SEND_TIMEOUT:
+        return Failure(
+            code: ResponeCode.SEND_TIMEOUT,
+            message: ResponeMessage.SEND_TIMEOUT);
+        break;
+      case DataSource.CACHE_ERROR:
+        return Failure(
+            code: ResponeCode.CACHE_ERROR, message: ResponeMessage.CACHE_ERROR);
+        break;
+      case DataSource.NO_INTERNET_CONNECTION:
+        return Failure(
+            code: ResponeCode.NO_INTERNET_CONNECTION,
+            message: ResponeMessage.NO_INTERNET_CONNECTION);
+        break;
+    }
+  }
 }
