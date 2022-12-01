7c1e6fad1af09058b16ba57c5674a88135805f8e
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-12-01 11:20:22 +0300

    Add new constants

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,5 +1,7 @@
 class Constants {
   static const String baseUrl = "https://alialsaeed777.mocklab.io/";
   static const String empty = "";
+  static const String token = "SEND TOKEN HERE";
   static const int zero = 0;
+  static const int api_timeout = 60000;
 }
