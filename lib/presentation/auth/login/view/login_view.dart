0b2d06407319366ae1c94ed852fb2ca47f033982
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-12-06 11:49:41 +0300

    Prepare view to bind it with viewmodel

--- lib/presentation/auth/login/view/login_view.dart
+++ lib/presentation/auth/login/view/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tera_store/presentation/auth/login/viewmodel/login_viewmodel.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -8,6 +9,27 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
+
+  TextEditingController _userNameController = TextEditingController();
+  TextEditingController _passowrdController = TextEditingController();
+
+  _bind() {
+    _viewModel.start(); // tell viewmodel to start your job
+    _userNameController.addListener(
+      () => _viewModel.setUserName(_userNameController.text),
+    );
+    _passowrdController.addListener(
+      () => _viewModel.setPassword(_passowrdController.text),
+    );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
