bfdc594c57dfa76b6e98f392c8dfb2780b791257
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-12-06 12:52:23 +0300

    Fininshing login UI

--- lib/presentation/auth/login/view/login_view.dart
+++ lib/presentation/auth/login/view/login_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tera_store/presentation/auth/login/viewmodel/login_viewmodel.dart';
+import 'package:tera_store/presentation/resources/assets_manager.dart';
+import 'package:tera_store/presentation/resources/colors_manager.dart';
+import 'package:tera_store/presentation/resources/strings_manager.dart';
+import 'package:tera_store/presentation/resources/values_manager.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -13,6 +17,8 @@ class _LoginViewState extends State<LoginView> {
 
   TextEditingController _userNameController = TextEditingController();
   TextEditingController _passowrdController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start(); // tell viewmodel to start your job
@@ -33,5 +39,90 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: AppPadding.p100),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Center(
+                  child: Image.asset(ImageAssets.splashLogo),
+                ),
+                SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outputIsUserNameValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _userNameController,
+                        decoration: InputDecoration(
+                          hintText: AppStrings.userName,
+                          labelText: AppStrings.userName,
+                          errorText: (snapshot.data ?? true)
+                              ? null
+                              : AppStrings.userNameError,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outputIsPasswordValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.visiblePassword,
+                        controller: _passowrdController,
+                        decoration: InputDecoration(
+                          hintText: AppStrings.password,
+                          labelText: AppStrings.password,
+                          errorText: (snapshot.data ?? true)
+                              ? null
+                              : AppStrings.passwordError,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outputIsPasswordValid,
+                    builder: (context, snapshot) {
+                      return ElevatedButton(
+                        onPressed: () {
+                          _viewModel.login();
+                        },
+                        child: Text(AppStrings.login),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
